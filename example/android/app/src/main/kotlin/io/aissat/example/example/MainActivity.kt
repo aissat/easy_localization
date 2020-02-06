a04eb92b50c4faf221c8ab71e9ab55700439a4e9
aissat <mr.aissat@gmail.com>
2020-02-06 20:38:38 +0100

    update example project

---
+++ example/android/app/src/main/kotlin/io/aissat/example/example/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.aissat.example.example
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+}
