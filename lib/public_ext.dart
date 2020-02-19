97309b849b6ac8ab0e239a4522ec6f7f10737465
aissat <mr.aissat@gmail.com>
2020-02-19 23:08:25 +0100

    update TranslateExtension Text widget

--- lib/public_ext.dart
+++ lib/public_ext.dart
@@ -2,8 +2,36 @@ import 'localization.dart';
 import 'package:flutter/widgets.dart';
 
 extension TranslateExtension on Text {
-  Text tr({List<String> args, String gender}) =>
-      Text(Localization.instance.tr(this.data, args: args, gender: gender));
-  Text plural(dynamic value) =>
-      Text(Localization.instance.plural(this.data, value));
+  Text tr({List<String> args, String gender}) => Text(
+        Localization.instance.tr(this.data, args: args, gender: gender),
+        key: this.key,
+        style: this.style,
+        strutStyle: this.strutStyle,
+        textAlign: this.textAlign,
+        textDirection: this.textDirection,
+        locale: this.locale,
+        softWrap: this.softWrap,
+        overflow: this.overflow,
+        textScaleFactor: this.textScaleFactor,
+        maxLines: this.maxLines,
+        semanticsLabel: this.semanticsLabel,
+        textWidthBasis: this.textWidthBasis,
+        textHeightBehavior: this.textHeightBehavior,
+      );
+  Text plural(dynamic value) => Text(
+        Localization.instance.plural(this.data, value),
+        key: this.key,
+        style: this.style,
+        strutStyle: this.strutStyle,
+        textAlign: this.textAlign,
+        textDirection: this.textDirection,
+        locale: this.locale,
+        softWrap: this.softWrap,
+        overflow: this.overflow,
+        textScaleFactor: this.textScaleFactor,
+        maxLines: this.maxLines,
+        semanticsLabel: this.semanticsLabel,
+        textWidthBasis: this.textWidthBasis,
+        textHeightBehavior: this.textHeightBehavior,
+      );
 }
