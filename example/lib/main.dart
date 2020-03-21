94d04547a0a60066ac882d150756c0e29fb564dd
Alexey Z <47769319+Overman775@users.noreply.github.com>
2020-03-21 23:32:32 +0500

    clean example log

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -73,7 +73,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    log(tr("title"), name: this.toString() );
     return Scaffold(
       appBar: AppBar(
         title: Text("title").tr(context: context),
