33c08868c52047298abcd8afdd9a48751032221a
aissat <mr.aissat@gmail.com>
2020-03-24 22:53:20 +0100

    updated EasyLocalization app

--- lib/src/easy_localization_app.dart
+++ lib/src/easy_localization_app.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:intl/intl_standalone.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -12,13 +11,11 @@ import 'asset_loader.dart';
 import 'localization.dart';
 
 part 'bloc/easy_localization_bloc.dart';
-part 'bloc/easy_localization_locale.dart';
 
 class EasyLocalization extends StatefulWidget {
   final Widget child;
   final List<Locale> supportedLocales;
   final Locale fallbackLocale;
-  final _EasyLocalizationDelegate delegate;
   final bool useOnlyLangCode;
   final String path;
   final AssetLoader assetLoader;
@@ -32,96 +29,165 @@ class EasyLocalization extends StatefulWidget {
     this.useOnlyLangCode = false,
     this.assetLoader = const RootBundleAssetLoader(),
     this.saveLocale = true,
-  })  : //assert(supportedLocales.contains(fallbackLocale)),
-        delegate = _EasyLocalizationDelegate(
-            path: path,
-            supportedLocales: supportedLocales,
-            useOnlyLangCode: useOnlyLangCode,
-            assetLoader: assetLoader),
-        super(key: key);
+  }) : super(key: key);
 
   _EasyLocalizationState createState() => _EasyLocalizationState();
 
-  static _EasyLocalizationState of(BuildContext context) =>
-      _EasyLocalizationProvider.of(context).data;
+  static _EasyLocalizationProvider of(BuildContext context) =>
+      _EasyLocalizationProvider.of(context);
 }
 
 class _EasyLocalizationState extends State<EasyLocalization> {
-  _EasyLocalizationLocale _ezlocale;
-  Locale _locale;
-  // var _streamSavedAppLocale;
-  EasyLocalizationBloc bloc;
-
-  Locale get locale => _locale;
-
-  set locale(Locale l) {
-    if (!supportedLocales.contains(l))
-      throw new Exception("Locale $l is not supported by this app.");
-    print(l);
-    _locale = l;
-    bloc.onChangeLocal(l);
-  }
-
-  List<Locale> get supportedLocales => widget.supportedLocales;
-  _EasyLocalizationDelegate get delegate => widget.delegate;
-  Locale get fallbackLocale => widget.fallbackLocale;
-  bool get saveLocale => widget.saveLocale;
-  //delete saved locale
-  void deleteSaveLocale() => _ezlocale.deleteSaveLocale();
+  Locale locale;
+  _EasyLocalizationDelegate delegate;
+  EasyLocalizationBloc bloc = EasyLocalizationBloc();
 
   @override
   void dispose() {
-    //_locale.dispose();
     bloc.dispose();
     super.dispose();
   }
 
   @override
   void initState() {
-    _ezlocale =
-        _EasyLocalizationLocale(fallbackLocale, supportedLocales, saveLocale);
-    bloc = EasyLocalizationBloc(_ezlocale);
-    bloc.onChangeLocal(null);
+    _init();
+    delegate = _EasyLocalizationDelegate(
+        path: widget.path,
+        supportedLocales: widget.supportedLocales,
+        useOnlyLangCode: widget.useOnlyLangCode,
+        assetLoader: widget.assetLoader,
+        onLocaleChanged: bloc.onChangeLocal);
 
     super.initState();
   }
 
+  _init() async {
+    Locale _savedLocale;
+    Locale _osLocale;
+    _savedLocale = await loadSavedLocale();
+    // Get Device Locale
+    _osLocale = await _getDeviceLocale();
+    log('easy localization: Device locale ${_osLocale.toString()}');
+    // If saved locale then get
+    if (_savedLocale != null && widget.saveLocale) {
+      log('easy localization: Saved locale loaded ${_savedLocale.toString()}');
+      locale = _savedLocale;
+    } else {
+      locale = widget.supportedLocales.firstWhere(
+          (locale) => _checkInitLocale(locale, _osLocale),
+          orElse: () => _getFallbackLocale(
+              widget.supportedLocales, widget.fallbackLocale));
+    }
+    bloc.onChangeLocal(locale);
+  }
+
+  bool _checkInitLocale(Locale locale, Locale _osLocale) {
+    // If suported locale not contain countryCode then check only languageCode
+    if (locale.countryCode == null) {
+      return (locale.languageCode == _osLocale.languageCode);
+    } else {
+      return (locale == _osLocale);
+    }
+  }
+
+  //Get fallback Locale
+  Locale _getFallbackLocale(
+      List<Locale> supportedLocales, Locale fallbackLocale) {
+    //If fallbackLocale not set then return first from supportedLocales
+    if (fallbackLocale != null) {
+      return fallbackLocale;
+    } else {
+      return supportedLocales.first;
+    }
+  }
+
+  // Get Device Locale
+  Future<Locale> _getDeviceLocale() async {
+    final String _deviceLocale = await findSystemLocale();
+    return _localeFromString(_deviceLocale);
+  }
+
+  Future<Locale> loadSavedLocale() async {
+    SharedPreferences _preferences = await SharedPreferences.getInstance();
+    var _strLocale = _preferences.getString('locale');
+    return locale = _strLocale != null ? _localeFromString(_strLocale) : null;
+    // TODO reload delegate, set on Material Widget
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    return StreamBuilder(
-        stream: bloc.outStream,
-        builder: (context, snapshot ) {
-          if (snapshot.hasData && snapshot.data != null) {
-            _locale = snapshot.data;
-            return _EasyLocalizationProvider(
-              data: this,
-              child: widget.child,
-            );
-          } else  {
-            return FutureErrorWidget(msg: snapshot.data);
-          }
-        });
+    return Container(
+      color: Colors.white,
+      child: StreamBuilder(
+          stream: bloc.outStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData && !snapshot.hasError) {
+              return _EasyLocalizationProvider(
+                widget,
+                snapshot.data,
+                child: widget.child,
+                onLocaleChanged: bloc.onChangeLocal,
+                delegate: delegate,
+              );
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return EmptyPreloaderWidget();
+            } else {
+              return FutureErrorWidget(msg: snapshot.error);
+            }
+          }),
+    );
   }
 }
 
 class _EasyLocalizationProvider extends InheritedWidget {
-  _EasyLocalizationProvider({Key key, this.child, this.data})
+  EasyLocalization parent;
+  final Locale _locale;
+  final ValueChanged<Locale> onLocaleChanged;
+
+  List<Locale> get supportedLocales => parent.supportedLocales;
+
+  final _EasyLocalizationDelegate delegate;
+
+  _EasyLocalizationProvider(this.parent, this._locale,
+      {Key key, Widget child, this.onLocaleChanged, this.delegate})
       : super(key: key, child: child);
-  final _EasyLocalizationState data;
-  final Widget child;
+
+  Locale get locale => _locale;
+  Locale get fallbackLocale => parent.fallbackLocale;
+
+  set locale(Locale locale) {
+    assert(parent.supportedLocales.contains(locale));
+    if (parent.saveLocale) _saveLocale(locale);
+    log('easy localization: Locale set ${locale.toString()}');
+    onLocaleChanged(locale);
+  }
+
+  _saveLocale(Locale locale) async {
+    SharedPreferences _preferences = await SharedPreferences.getInstance();
+    await _preferences.setString('locale', locale.toString());
+    log('easy localization: Locale saved ${locale.toString()}');
+  }
+
+  deleteSaveLocale() async {
+    SharedPreferences _preferences = await SharedPreferences.getInstance();
+    await _preferences.setString('locale', null);
+    log('easy localization: Saved locale deleted');
+  }
+
+  @override
+  bool updateShouldNotify(_EasyLocalizationProvider oldWidget) {
+    return oldWidget._locale != this._locale;
+  }
 
   static _EasyLocalizationProvider of(BuildContext context) =>
       context.dependOnInheritedWidgetOfExactType<_EasyLocalizationProvider>();
-
-  @override
-  bool updateShouldNotify(_EasyLocalizationProvider oldWidget) => true;
 }
 
 class _EasyLocalizationDelegate extends LocalizationsDelegate<Localization> {
   final String path;
   final AssetLoader assetLoader;
   final List<Locale> supportedLocales;
+  final ValueChanged<Locale> onLocaleChanged;
 
   ///  * use only the lang code to generate i18n file path like en.json or ar.json
   final bool useOnlyLangCode;
@@ -130,7 +196,8 @@ class _EasyLocalizationDelegate extends LocalizationsDelegate<Localization> {
       {@required this.path,
       @required this.supportedLocales,
       this.useOnlyLangCode = false,
-      this.assetLoader});
+      this.assetLoader,
+      this.onLocaleChanged});
 
   @override
   bool isSupported(Locale locale) => supportedLocales.contains(locale);
