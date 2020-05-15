a076cccebdb428ce2b7f2be14def6b102b914fd3
aissat <mr.aissat@gmail.com>
2020-05-15 01:36:42 +0100

    📖 added utils API doc

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,5 +1,6 @@
 part of 'easy_localization_app.dart';
 
+/// convert string [localeString] returns the locale.
 Locale localeFromString(String localeString) {
   final localeList = localeString.split('_');
   switch (localeList.length) {
@@ -15,18 +16,20 @@ Locale localeFromString(String localeString) {
   }
 }
 
+/// convert [locale] to Srting at matches of [separator] and returns a string representing the locale.
 String localeToString(Locale locale, {String separator = '_'}) {
   return locale.toString().split('_').join(separator);
 }
-
+/// Emit a [info] log event
 void printInfo(String info) {
   print('\u001b[32mEasy Localization: $info\u001b[0m');
 }
 
+/// Emit a [warning] log event
 void printWarning(String warning) {
   print('\u001B[34m[WARNING] Easy Localization: $warning\u001b[0m');
 }
-
+/// Emit a [error] log event
 void printError(String error) {
   print('\u001b[31m[ERROR] Easy Localization: $error\u001b[0m');
 }
