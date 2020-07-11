ac01dbe8a9469721a5824774217c77c9a3c4c505
Alexey Z <47769319+Overman775@users.noreply.github.com>
2020-07-11 16:34:50 +0500

    Updated pubspec, Added argument in codegen

--- example/lib/generated/codegen_loader.g.dart
+++ example/lib/generated/codegen_loader.g.dart
@@ -1,7 +1,10 @@
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: prefer_single_quotes
+
 import 'dart:ui';
 
-import 'package:easy_localization/src/asset_loader.dart';
+import 'package:easy_localization/easy_localization.dart' show AssetLoader;
 
 class CodegenLoader extends AssetLoader{
   const CodegenLoader();
@@ -11,82 +14,6 @@ class CodegenLoader extends AssetLoader{
     return Future.value(mapLocales[locale.toString()]);
   }
 
-    static const Map<String,dynamic> de = {
-  "title": "Hallo",
-  "msg": "Hallo {} in der {} welt ",
-  "msg_named": "{} ist in {lang} geschrieben",
-  "clickMe": "Click mich",
-  "profile": {
-    "reset_password": {
-      "label": "Password zurücksetzten",
-      "username": "Name",
-      "password": "Password"
-    }
-  },
-  "clicked": {
-    "zero": "Du hast {} mal geklickt",
-    "one": "Du hast {} mal geklickt",
-    "two": "Du hast {} mal geklickt",
-    "few": "Du hast {} mal geklickt",
-    "many": "Du hast {} mal geklickt",
-    "other": "Du hast {} mal geklickt"
-  },
-  "amount": {
-    "zero": "Deine Klicks: {}",
-    "one": "Deine Klicks: {}",
-    "two": "Deine Klicks: {}",
-    "few": "Deine Klicks: {}",
-    "many": "Deine Klicks: {}",
-    "other": "Deine Klicks: {}"
-  },
-  "gender": {
-    "male": "Hi Mann ;) ",
-    "female": "Hallo Frau :)",
-    "with_arg": {
-      "male": "Hi Mann ;) {}",
-      "female": "Hallo Frau :) {}"
-    }
-  },
-  "reset_locale": "Gespeicherte Sprache zurücksettzen"
-};
-  static const Map<String,dynamic> ru = {
-  "title": "Привет!",
-  "msg": "Привет! {} добро пожаловать {} мир! ",
-  "msg_named": "{} написан на языке {lang}",
-  "clickMe": "Нажми на меня",
-  "profile": {
-    "reset_password": {
-      "label": "Сбросить пароль",
-      "username": "Логин",
-      "password": "Пароль"
-    }
-  },
-  "clicked": {
-    "zero": "Ты кликнул {} раз!",
-    "one": "Ты кликнул {} раз!",
-    "two": "Ты кликнул {} раза!",
-    "few": "Ты кликнул {} раз!",
-    "many": "Ты кликнул {} раз!",
-    "other": "Ты кликнул {} раз!"
-  },
-  "amount": {
-    "zero": "Твой счет : {} ",
-    "one": "Твой счет : {} ",
-    "two": "Твой счет : {} ",
-    "few": "Твой счет : {} ",
-    "many": "Твой счет : {} ",
-    "other": "Твой счет : {} "
-  },
-  "gender": {
-    "male": "Привет мужык ;) ",
-    "female": "Привет девчуля :)",
-    "with_arg": {
-      "male": "Привет мужык ;) {}",
-      "female": "Привет девчуля :) {}"
-    }
-  },
-  "reset_locale": "Сбросить сохраненный язык"
-};
   static const Map<String,dynamic> ar_DZ = {
   "title": "السلام",
   "msg": "السلام عليكم يا {} في عالم {}",
@@ -125,159 +52,7 @@ class CodegenLoader extends AssetLoader{
   },
   "reset_locale": "إعادة تعيين اللغة المحفوظة"
 };
-  static const Map<String,dynamic> en = {
-  "title": "Hello",
-  "msg": "Hello {} in the {} world ",
-  "msg_named": "{} are written in the {lang} language",
-  "clickMe": "Click me",
-  "profile": {
-    "reset_password": {
-      "label": "Reset Password",
-      "username": "Username",
-      "password": "password"
-    }
-  },
-  "clicked": {
-    "zero": "You clicked {} times!",
-    "one": "You clicked {} time!",
-    "two": "You clicked {} times!",
-    "few": "You clicked {} times!",
-    "many": "You clicked {} times!",
-    "other": "You clicked {} times!"
-  },
-  "amount": {
-    "zero": "Your amount : {} ",
-    "one": "Your amount : {} ",
-    "two": "Your amount : {} ",
-    "few": "Your amount : {} ",
-    "many": "Your amount : {} ",
-    "other": "Your amount : {} "
-  },
-  "gender": {
-    "male": "Hi man ;) ",
-    "female": "Hello girl :)",
-    "with_arg": {
-      "male": "Hi man ;) {}",
-      "female": "Hello girl :) {}"
-    }
-  },
-  "reset_locale": "Reset Saved Language"
-};
-  static const Map<String,dynamic> de_DE = {
-  "title": "Hallo",
-  "msg": "Hallo {} in der {} welt ",
-  "msg_named": "{} ist in {lang} geschrieben",
-  "clickMe": "Click mich",
-  "profile": {
-    "reset_password": {
-      "label": "Password zurücksetzten",
-      "username": "Name",
-      "password": "Password"
-    }
-  },
-  "clicked": {
-    "zero": "Du hast {} mal geklickt",
-    "one": "Du hast {} mal geklickt",
-    "two": "Du hast {} mal geklickt",
-    "few": "Du hast {} mal geklickt",
-    "many": "Du hast {} mal geklickt",
-    "other": "Du hast {} mal geklickt"
-  },
-  "amount": {
-    "zero": "Deine Klicks: {}",
-    "one": "Deine Klicks: {}",
-    "two": "Deine Klicks: {}",
-    "few": "Deine Klicks: {}",
-    "many": "Deine Klicks: {}",
-    "other": "Deine Klicks: {}"
-  },
-  "gender": {
-    "male": "Hi Mann ;) ",
-    "female": "Hallo Frau :)",
-    "with_arg": {
-      "male": "Hi Mann ;) {}",
-      "female": "Hallo Frau :) {}"
-    }
-  },
-  "reset_locale": "Gespeicherte Sprache zurücksettzen"
-};
-  static const Map<String,dynamic> ru_RU = {
-  "title": "Привет!",
-  "msg": "Привет! {} добро пожаловать {} мир! ",
-  "msg_named": "{} написан на языке {lang}",
-  "clickMe": "Нажми на меня",
-  "profile": {
-    "reset_password": {
-      "label": "Сбросить пароль",
-      "username": "Логин",
-      "password": "Пароль"
-    }
-  },
-  "clicked": {
-    "zero": "Ты кликнул {} раз!",
-    "one": "Ты кликнул {} раз!",
-    "two": "Ты кликнул {} раза!",
-    "few": "Ты кликнул {} раз!",
-    "many": "Ты кликнул {} раз!",
-    "other": "Ты кликнул {} раз!"
-  },
-  "amount": {
-    "zero": "Твой счет : {} ",
-    "one": "Твой счет : {} ",
-    "two": "Твой счет : {} ",
-    "few": "Твой счет : {} ",
-    "many": "Твой счет : {} ",
-    "other": "Твой счет : {} "
-  },
-  "gender": {
-    "male": "Привет мужык ;) ",
-    "female": "Привет девчуля :)",
-    "with_arg": {
-      "male": "Привет мужык ;) {}",
-      "female": "Привет девчуля :) {}"
-    }
-  },
-  "reset_locale": "Сбросить сохраненный язык"
-};
-  static const Map<String,dynamic> en_US = {
-  "title": "Hello",
-  "msg": "Hello {} in the {} world ",
-  "msg_named": "{} are written in the {lang} language",
-  "clickMe": "Click me",
-  "profile": {
-    "reset_password": {
-      "label": "Reset Password",
-      "username": "Username",
-      "password": "password"
-    }
-  },
-  "clicked": {
-    "zero": "You clicked {} times!",
-    "one": "You clicked {} time!",
-    "two": "You clicked {} times!",
-    "few": "You clicked {} times!",
-    "many": "You clicked {} times!",
-    "other": "You clicked {} times!"
-  },
-  "amount": {
-    "zero": "Your amount : {} ",
-    "one": "Your amount : {} ",
-    "two": "Your amount : {} ",
-    "few": "Your amount : {} ",
-    "many": "Your amount : {} ",
-    "other": "Your amount : {} "
-  },
-  "gender": {
-    "male": "Hi man ;) ",
-    "female": "Hello girl :)",
-    "with_arg": {
-      "male": "Hi man ;) {}",
-      "female": "Hello girl :) {}"
-    }
-  },
-  "reset_locale": "Reset Saved Language"
-};
-  static const Map<String,dynamic> ar = {
+static const Map<String,dynamic> ar = {
   "title": "السلام",
   "msg": "السلام عليكم يا {} في عالم {}",
   "msg_named": "{} مكتوبة باللغة {lang}",
@@ -315,5 +90,233 @@ class CodegenLoader extends AssetLoader{
   },
   "reset_locale": "إعادة تعيين اللغة المحفوظة"
 };
-  static const Map<String, Map<String,dynamic>> mapLocales = {"de": de, "ru": ru, "ar_DZ": ar_DZ, "en": en, "de_DE": de_DE, "ru_RU": ru_RU, "en_US": en_US, "ar": ar};
+static const Map<String,dynamic> de_DE = {
+  "title": "Hallo",
+  "msg": "Hallo {} in der {} welt ",
+  "msg_named": "{} ist in {lang} geschrieben",
+  "clickMe": "Click mich",
+  "profile": {
+    "reset_password": {
+      "label": "Password zurücksetzten",
+      "username": "Name",
+      "password": "Password"
+    }
+  },
+  "clicked": {
+    "zero": "Du hast {} mal geklickt",
+    "one": "Du hast {} mal geklickt",
+    "two": "Du hast {} mal geklickt",
+    "few": "Du hast {} mal geklickt",
+    "many": "Du hast {} mal geklickt",
+    "other": "Du hast {} mal geklickt"
+  },
+  "amount": {
+    "zero": "Deine Klicks: {}",
+    "one": "Deine Klicks: {}",
+    "two": "Deine Klicks: {}",
+    "few": "Deine Klicks: {}",
+    "many": "Deine Klicks: {}",
+    "other": "Deine Klicks: {}"
+  },
+  "gender": {
+    "male": "Hi Mann ;) ",
+    "female": "Hallo Frau :)",
+    "with_arg": {
+      "male": "Hi Mann ;) {}",
+      "female": "Hallo Frau :) {}"
+    }
+  },
+  "reset_locale": "Gespeicherte Sprache zurücksettzen"
+};
+static const Map<String,dynamic> de = {
+  "title": "Hallo",
+  "msg": "Hallo {} in der {} welt ",
+  "msg_named": "{} ist in {lang} geschrieben",
+  "clickMe": "Click mich",
+  "profile": {
+    "reset_password": {
+      "label": "Password zurücksetzten",
+      "username": "Name",
+      "password": "Password"
+    }
+  },
+  "clicked": {
+    "zero": "Du hast {} mal geklickt",
+    "one": "Du hast {} mal geklickt",
+    "two": "Du hast {} mal geklickt",
+    "few": "Du hast {} mal geklickt",
+    "many": "Du hast {} mal geklickt",
+    "other": "Du hast {} mal geklickt"
+  },
+  "amount": {
+    "zero": "Deine Klicks: {}",
+    "one": "Deine Klicks: {}",
+    "two": "Deine Klicks: {}",
+    "few": "Deine Klicks: {}",
+    "many": "Deine Klicks: {}",
+    "other": "Deine Klicks: {}"
+  },
+  "gender": {
+    "male": "Hi Mann ;) ",
+    "female": "Hallo Frau :)",
+    "with_arg": {
+      "male": "Hi Mann ;) {}",
+      "female": "Hallo Frau :) {}"
+    }
+  },
+  "reset_locale": "Gespeicherte Sprache zurücksettzen"
+};
+static const Map<String,dynamic> en_US = {
+  "title": "Hello",
+  "msg": "Hello {} in the {} world ",
+  "msg_named": "{} are written in the {lang} language",
+  "clickMe": "Click me",
+  "profile": {
+    "reset_password": {
+      "label": "Reset Password",
+      "username": "Username",
+      "password": "password"
+    }
+  },
+  "clicked": {
+    "zero": "You clicked {} times!",
+    "one": "You clicked {} time!",
+    "two": "You clicked {} times!",
+    "few": "You clicked {} times!",
+    "many": "You clicked {} times!",
+    "other": "You clicked {} times!"
+  },
+  "amount": {
+    "zero": "Your amount : {} ",
+    "one": "Your amount : {} ",
+    "two": "Your amount : {} ",
+    "few": "Your amount : {} ",
+    "many": "Your amount : {} ",
+    "other": "Your amount : {} "
+  },
+  "gender": {
+    "male": "Hi man ;) ",
+    "female": "Hello girl :)",
+    "with_arg": {
+      "male": "Hi man ;) {}",
+      "female": "Hello girl :) {}"
+    }
+  },
+  "reset_locale": "Reset Saved Language"
+};
+static const Map<String,dynamic> en = {
+  "title": "Hello",
+  "msg": "Hello {} in the {} world ",
+  "msg_named": "{} are written in the {lang} language",
+  "clickMe": "Click me",
+  "profile": {
+    "reset_password": {
+      "label": "Reset Password",
+      "username": "Username",
+      "password": "password"
+    }
+  },
+  "clicked": {
+    "zero": "You clicked {} times!",
+    "one": "You clicked {} time!",
+    "two": "You clicked {} times!",
+    "few": "You clicked {} times!",
+    "many": "You clicked {} times!",
+    "other": "You clicked {} times!"
+  },
+  "amount": {
+    "zero": "Your amount : {} ",
+    "one": "Your amount : {} ",
+    "two": "Your amount : {} ",
+    "few": "Your amount : {} ",
+    "many": "Your amount : {} ",
+    "other": "Your amount : {} "
+  },
+  "gender": {
+    "male": "Hi man ;) ",
+    "female": "Hello girl :)",
+    "with_arg": {
+      "male": "Hi man ;) {}",
+      "female": "Hello girl :) {}"
+    }
+  },
+  "reset_locale": "Reset Saved Language"
+};
+static const Map<String,dynamic> ru_RU = {
+  "title": "Привет!",
+  "msg": "Привет! {} добро пожаловать {} мир! ",
+  "msg_named": "{} написан на языке {lang}",
+  "clickMe": "Нажми на меня",
+  "profile": {
+    "reset_password": {
+      "label": "Сбросить пароль",
+      "username": "Логин",
+      "password": "Пароль"
+    }
+  },
+  "clicked": {
+    "zero": "Ты кликнул {} раз!",
+    "one": "Ты кликнул {} раз!",
+    "two": "Ты кликнул {} раза!",
+    "few": "Ты кликнул {} раз!",
+    "many": "Ты кликнул {} раз!",
+    "other": "Ты кликнул {} раз!"
+  },
+  "amount": {
+    "zero": "Твой счет : {} ",
+    "one": "Твой счет : {} ",
+    "two": "Твой счет : {} ",
+    "few": "Твой счет : {} ",
+    "many": "Твой счет : {} ",
+    "other": "Твой счет : {} "
+  },
+  "gender": {
+    "male": "Привет мужык ;) ",
+    "female": "Привет девчуля :)",
+    "with_arg": {
+      "male": "Привет мужык ;) {}",
+      "female": "Привет девчуля :) {}"
+    }
+  },
+  "reset_locale": "Сбросить сохраненный язык"
+};
+static const Map<String,dynamic> ru = {
+  "title": "Привет!",
+  "msg": "Привет! {} добро пожаловать {} мир! ",
+  "msg_named": "{} написан на языке {lang}",
+  "clickMe": "Нажми на меня",
+  "profile": {
+    "reset_password": {
+      "label": "Сбросить пароль",
+      "username": "Логин",
+      "password": "Пароль"
+    }
+  },
+  "clicked": {
+    "zero": "Ты кликнул {} раз!",
+    "one": "Ты кликнул {} раз!",
+    "two": "Ты кликнул {} раза!",
+    "few": "Ты кликнул {} раз!",
+    "many": "Ты кликнул {} раз!",
+    "other": "Ты кликнул {} раз!"
+  },
+  "amount": {
+    "zero": "Твой счет : {} ",
+    "one": "Твой счет : {} ",
+    "two": "Твой счет : {} ",
+    "few": "Твой счет : {} ",
+    "many": "Твой счет : {} ",
+    "other": "Твой счет : {} "
+  },
+  "gender": {
+    "male": "Привет мужык ;) ",
+    "female": "Привет девчуля :)",
+    "with_arg": {
+      "male": "Привет мужык ;) {}",
+      "female": "Привет девчуля :) {}"
+    }
+  },
+  "reset_locale": "Сбросить сохраненный язык"
+};
+static const Map<String, Map<String,dynamic>> mapLocales = {"ar_DZ": ar_DZ, "ar": ar, "de_DE": de_DE, "de": de, "en_US": en_US, "en": en, "ru_RU": ru_RU, "ru": ru};
 }
