42e4c4c886158af4712d5d1ef5b6de6a2e138efe
Alexey Zdorovykh <overman775@users.noreply.github.com>
2021-01-10 16:40:25 +0500

    EasyLogger package

---
+++ packages/easy_logger/lib/src/enums.dart
@@ -0,0 +1,26 @@
+/// Build mode enum for
+enum BuildMode {
+  /// Release build mode
+  release,
+
+  /// Profile build mode
+  profile,
+
+  /// Debug build mode
+  debug
+}
+
+/// Levels messages
+enum EasyLoggerLevel {
+  ///Debug level message
+  debug,
+
+  ///Information level message
+  info,
+
+  ///Warning level message
+  warning,
+
+  ///Error level message
+  error,
+}
---
+++ packages/easy_logger/lib/src/logger.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/foundation.dart';
+
+import 'enums.dart';
+import 'logger_printer.dart';
+
+/// Easy Logger
+class EasyLogger {
+  EasyLogger(
+      {this.name,
+      this.enableBuildModes = const <BuildMode>[
+        BuildMode.profile,
+        BuildMode.debug,
+      ],
+      this.enableLevels = const <EasyLoggerLevel>[
+        EasyLoggerLevel.debug,
+        EasyLoggerLevel.info,
+        EasyLoggerLevel.error,
+        EasyLoggerLevel.warning,
+      ],
+      EasyLogPrinter printer}) {
+    _printer = printer ?? easyLogDefaultPrinter;
+    _currentBuildMode = _getCurrentBuildMode();
+  }
+
+  BuildMode _currentBuildMode;
+  String name;
+  List<BuildMode> enableBuildModes;
+  List<EasyLoggerLevel> enableLevels;
+
+  EasyLogPrinter _printer;
+  EasyLogPrinter get printer => _printer;
+  set printer(EasyLogPrinter newPrinter) => _printer = newPrinter;
+
+  BuildMode _getCurrentBuildMode() {
+    if (kReleaseMode) {
+      return BuildMode.release;
+    } else if (kProfileMode) {
+      return BuildMode.profile;
+    }
+    return BuildMode.debug;
+  }
+
+  bool isEnabled(EasyLoggerLevel level) {
+    if (!enableBuildModes.contains(_currentBuildMode)) {
+      return false;
+    }
+    if (!enableLevels.contains(level)) {
+      return false;
+    }
+    return true;
+  }
+
+  void call(Object object, {StackTrace stackTrace, EasyLoggerLevel level = EasyLoggerLevel.info}) {
+    if (isEnabled(level)) {
+      _printer(object, stackTrace: stackTrace, level: level, name: name);
+    }
+  }
+
+  void debug(Object object, {StackTrace stackTrace}) =>
+      call(object, stackTrace: stackTrace, level: EasyLoggerLevel.debug);
+
+  void info(Object object, {StackTrace stackTrace}) =>
+      call(object, stackTrace: stackTrace, level: EasyLoggerLevel.info);
+
+  void warning(Object object, {StackTrace stackTrace}) =>
+      call(object, stackTrace: stackTrace, level: EasyLoggerLevel.warning);
+
+  void error(Object object, {StackTrace stackTrace}) =>
+      call(object, stackTrace: stackTrace, level: EasyLoggerLevel.error);
+}
