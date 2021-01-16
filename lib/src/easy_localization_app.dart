d6781aab593806338f528c236f685c3417e86951
Alexey Zdorovykh <overman775@users.noreply.github.com>
2021-01-17 00:21:58 +0500

    update tests

--- lib/src/easy_localization_app.dart
+++ lib/src/easy_localization_app.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:easy_localization/easy_localization.dart';
 import 'package:easy_localization/src/easy_localization_controller.dart';
 import 'package:easy_logger/easy_logger.dart';
 import 'package:flutter/material.dart';
