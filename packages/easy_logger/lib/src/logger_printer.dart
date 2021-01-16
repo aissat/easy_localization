c2ee809d131aa520ad8da80e6dbe3f0803dc7563
Alexey Zdorovykh <overman775@users.noreply.github.com>
2021-01-16 20:25:26 +0500

    update printer

--- packages/easy_logger/lib/src/logger_printer.dart
+++ packages/easy_logger/lib/src/logger_printer.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import '../easy_logger.dart';
 
 /// Type for function printing/logging in [EasyLogger].
@@ -7,29 +5,46 @@ typedef EasyLogPrinter = Function(Object object, {String name, LevelMessages lev
 
 /// Default function printing.
 EasyLogPrinter easyLogDefaultPrinter = (Object object, {String name, StackTrace stackTrace, LevelMessages level}) {
-  String _prepareString() {
+  String _coloredString(String string) {
     switch (level) {
       case LevelMessages.debug:
         // white
-        return '\u001b[37m[INFO] ${object.toString()}\u001b[0m';
+        return '\u001b[37m$string\u001b[0m';
       case LevelMessages.info:
         // green
-        return '\u001b[32m[INFO] ${object.toString()}\u001b[0m';
+        return '\u001b[32m$string\u001b[0m';
       case LevelMessages.warning:
         // blue
-        return '\u001B[34m[WARNING] ${object.toString()}\u001b[0m';
+        return '\u001B[34m$string\u001b[0m';
       case LevelMessages.error:
         // red
-        return '\u001b[31m[ERROR] ${object.toString()}\u001b[0m';
+        return '\u001b[31m$string\u001b[0m';
       default:
         // gray
-        return '\u001b[90m${object.toString()}\u001b[0m';
+        return '\u001b[90m$string\u001b[0m';
     }
   }
 
-  log(
-    _prepareString(),
-    name: name,
-    stackTrace: stackTrace,
-  );
+  String _prepareObject() {
+    switch (level) {
+      case LevelMessages.debug:
+        return _coloredString('[$name] [DEBUG] ${object.toString()}');
+      case LevelMessages.info:
+        return _coloredString('[$name] [INFO] ${object.toString()}');
+      case LevelMessages.warning:
+        return _coloredString('[$name] [WARNING] ${object.toString()}');
+      case LevelMessages.error:
+        return _coloredString('[$name] [ERROR] ${object.toString()}');
+      default:
+        return _coloredString('[$name] ${object.toString()}');
+    }
+  }
+
+  print(_prepareObject());
+
+  if (stackTrace != null) {
+    String stackTraceString;
+    stackTraceString = '__________________________________\n${stackTrace.toString()}';
+    print(_coloredString(stackTraceString));
+  }
 };
