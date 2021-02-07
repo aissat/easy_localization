bac88371bf77b0f465594da8ba8ecab549f789f6
Alexey Zdorovykh <overman775@users.noreply.github.com>
2021-02-07 18:23:33 +0500

    update example

--- example/lib/lang_view.dart
+++ example/lib/lang_view.dart
@@ -36,54 +36,72 @@ class LanguageView extends StatelessWidget {
                 ),
               ),
             ),
-            buildSwitchListTileMenuItem(
-                context: context,
+            _SwitchListTileMenuItem(
                 title: 'عربي',
                 subtitle: 'عربي',
                 locale:
                     context.supportedLocales[1] //BuildContext extension method
                 ),
-            buildDivider(),
-            buildSwitchListTileMenuItem(
-                context: context,
+            _Divider(),
+            _SwitchListTileMenuItem(
                 title: 'English',
                 subtitle: 'English',
-                locale: EasyLocalization.of(context).supportedLocales[0]),
-            buildDivider(),
-            buildSwitchListTileMenuItem(
-                context: context,
+                locale: context.supportedLocales[0]),
+            _Divider(),
+            _SwitchListTileMenuItem(
                 title: 'German',
                 subtitle: 'German',
-                locale: EasyLocalization.of(context).supportedLocales[2]),
-            buildDivider(),
-            buildSwitchListTileMenuItem(
-                context: context,
+                locale: context.supportedLocales[2]),
+            _Divider(),
+            _SwitchListTileMenuItem(
                 title: 'Русский',
                 subtitle: 'Русский',
-                locale: EasyLocalization.of(context).supportedLocales[3]),
-            buildDivider(),
+                locale: context.supportedLocales[3]),
+            _Divider(),
           ],
         ),
       ),
     );
   }
+}
 
-  Container buildDivider() => Container(
-        margin: EdgeInsets.symmetric(
-          horizontal: 24,
-        ),
-        child: Divider(
-          color: Colors.grey,
-        ),
-      );
+class _Divider extends StatelessWidget {
+  const _Divider({Key key}) : super(key: key);
 
-  Container buildSwitchListTileMenuItem(
-      {BuildContext context, String title, String subtitle, Locale locale}) {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(
-        left: 10,
-        right: 10,
-        top: 5,
+      margin: EdgeInsets.symmetric(
+        horizontal: 24,
+      ),
+      child: Divider(
+        color: Colors.grey,
+      ),
+    );
+  }
+}
+
+class _SwitchListTileMenuItem extends StatelessWidget {
+  const _SwitchListTileMenuItem({
+    Key key,
+    this.title,
+    this.subtitle,
+    this.locale,
+  }) : super(key: key);
+
+  final String title;
+  final String subtitle;
+  final Locale locale;
+
+  bool isSelected(BuildContext context) => locale == context.locale;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10, top: 5),
+      decoration: BoxDecoration(
+        border:
+            isSelected(context) ? Border.all(color: Colors.blueAccent) : null,
       ),
       child: ListTile(
           dense: true,
@@ -97,7 +115,6 @@ class LanguageView extends StatelessWidget {
           onTap: () async {
             log(locale.toString(), name: toString());
             await context.setLocale(locale); //BuildContext extension method
-            //EasyLocalization.of(context).locale = locale;
             Navigator.pop(context);
           }),
     );
