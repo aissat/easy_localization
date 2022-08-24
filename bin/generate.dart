aissat/easy_localization

commit 51ddbb3ef6a244998178df2c78511ad50f70464d

Add option allowing skip keys of nested object

bin/generate.dart
@@ -73,6 +73,14 @@ ArgParser _generateArgParser(GenerateOptions? generateOptions) {
       help: 'Support json or keys formats',
       allowed: ['json', 'keys']);
 
+  parser.addFlag(
+    'skip-object-keys',
+    abbr: 'x',
+    defaultsTo: false,
+    callback: (bool? x) => generateOptions!.skipObjectKeys = x,
+    help: 'If true - skip keys of nested objects.',
+  );
+
   return parser;
 }
 
@@ -83,10 +91,11 @@ class GenerateOptions {
   String? outputDir;
   String? outputFile;
   String? format;
+  bool? skipObjectKeys;
 
   @override
   String toString() {
-    return 'format: $format sourceDir: $sourceDir sourceFile: $sourceFile outputDir: $outputDir outputFile: $outputFile';
+    return 'format: $format sourceDir: $sourceDir sourceFile: $sourceFile outputDir: $outputDir outputFile: $outputFile skipObjectKeys: $skipObjectKeys';
   }
 }
 
@@ -117,7 +126,7 @@ void handleLangFiles(GenerateOptions options) async {
   }
 
   if (files.isNotEmpty) {
-    generateFile(files, outputPath, options.format);
+    generateFile(files, outputPath, options);
   } else {
     printError('Source path empty');
   }
@@ -132,8 +141,8 @@ Future<List<FileSystemEntity>> dirContents(Directory dir) {
   return completer.future;
 }
 
-void generateFile(
-    List<FileSystemEntity> files, Directory outputPath, String? format) async {
+void generateFile(List<FileSystemEntity> files, Directory outputPath,
+    GenerateOptions options) async {
   var generatedFile = File(outputPath.path);
   if (!generatedFile.existsSync()) {
     generatedFile.createSync(recursive: true);
@@ -141,12 +150,12 @@ void generateFile(
 
   var classBuilder = StringBuffer();
 
-  switch (format) {
+  switch (options.format) {
     case 'json':
       await _writeJson(classBuilder, files);
       break;
     case 'keys':
-      await _writeKeys(classBuilder, files);
+      await _writeKeys(classBuilder, files, options.skipObjectKeys);
       break;
     // case 'csv':
     //   await _writeCsv(classBuilder, files);
@@ -161,8 +170,8 @@ void generateFile(
   printInfo('All done! File generated in ${outputPath.path}');
 }
 
-Future _writeKeys(
-    StringBuffer classBuilder, List<FileSystemEntity> files) async {
+Future _writeKeys(StringBuffer classBuilder, List<FileSystemEntity> files,
+    bool? skipObjectKeys) async {
   var file = '''
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
 
@@ -174,31 +183,45 @@ abstract class  LocaleKeys {
   Map<String, dynamic> translations =
       json.decode(await fileData.readAsString());
 
-  file += _resolve(translations);
+  file += _resolve(translations, skipObjectKeys);
 
   classBuilder.writeln(file);
 }
 
-String _resolve(Map<String, dynamic> translations, [String? accKey]) {
+String _resolve(Map<String, dynamic> translations, bool? skipObjectKeys,
+    [String? accKey]) {
   var fileContent = '';
 
   final sortedKeys = translations.keys.toList();
 
+  final canIgnoreKeys = skipObjectKeys != null && skipObjectKeys;
+
+  bool containsPreservedKeywords(Map<String, dynamic> map) =>
+      map.keys.any((element) => _preservedKeywords.contains(element));
+
   for (var key in sortedKeys) {
+    var ignoreKey = false;
     if (translations[key] is Map) {
+      // If key does not contain keys for plural(), gender() etc. and option is enabled -> ignore it
+      ignoreKey = !containsPreservedKeywords(
+              translations[key] as Map<String, dynamic>) &&
+          canIgnoreKeys;
+
       var nextAccKey = key;
       if (accKey != null) {
         nextAccKey = '$accKey.$key';
       }
 
-      fileContent += _resolve(translations[key], nextAccKey);
+      fileContent += _resolve(translations[key], skipObjectKeys, nextAccKey);
     }
 
     if (!_preservedKeywords.contains(key)) {
-      accKey != null
+      accKey != null && !ignoreKey
           ? fileContent +=
               '  static const ${accKey.replaceAll('.', '_')}\_$key = \'$accKey.$key\';\n'
-          : fileContent += '  static const $key = \'$key\';\n';
+          : !ignoreKey
+              ? fileContent += '  static const $key = \'$key\';\n'
+              : null;
     }
   }
 

commit 828aa6a8d1cd96fbf439e23a968181603e4e514e

Update generate.dart
Fixed https://github.com/aissat/easy_localization/issues/513:
Bug(s) introduced with https://github.com/aissat/easy_localization/commit/51ddbb3ef6a244998178df2c78511ad50f70464d due to a wrong implementation of "--[no]-skip-unnecessary-keys" flag.

Fixed https://github.com/aissat/easy_localization/issues/492:
Bug introduced in https://github.com/aissat/easy_localization/commit/8048ff5c1739fa4932f324d00215e4be6fb97f7b by replacing all print calls with log following linter's suggestion. This approach is problematic because log won't print anything in production.
To fix was enough to replace all log calls with writing on stdout/stderr.

bin/generate.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:developer';
 import 'dart:io';
 
 import 'package:args/args.dart';
@@ -31,7 +30,7 @@ bool _isHelpCommand(List<String> args) {
 
 void _printHelperDisplay() {
   var parser = _generateArgParser(null);
-  log(parser.usage);
+  stdout.writeln(parser.usage);
 }
 
 GenerateOptions _generateOption(List<String> args) {
@@ -195,35 +194,36 @@ String _resolve(Map<String, dynamic> translations, bool? skipUnnecessaryKeys,
 
   final sortedKeys = translations.keys.toList();
 
-  final canIgnoreKeys = skipUnnecessaryKeys == true;
+  final canIgnoreKeys = skipUnnecessaryKeys == true && accKey != null;
 
   bool containsPreservedKeywords(Map<String, dynamic> map) =>
       map.keys.any((element) => _preservedKeywords.contains(element));
 
   for (var key in sortedKeys) {
-    var ignoreKey = false;
-    if (translations[key] is Map) {
-      // If key does not contain keys for plural(), gender() etc. and option is enabled -> ignore it
-      ignoreKey = !containsPreservedKeywords(
-              translations[key] as Map<String, dynamic>) &&
-          canIgnoreKeys;
+    var ignoreKey = canIgnoreKeys;
 
-      var nextAccKey = key;
-      if (accKey != null) {
-        nextAccKey = '$accKey.$key';
-      }
+    if (translations[key] is Map) {
+      ignoreKey &=
+          !containsPreservedKeywords(translations[key] as Map<String, dynamic>);
+
+      var nextAccKey = (accKey == null ? key : '$accKey.$key');
 
       fileContent +=
           _resolve(translations[key], skipUnnecessaryKeys, nextAccKey);
+    } else {
+      ignoreKey &= !_preservedKeywords.contains(key);
     }
 
-    if (!_preservedKeywords.contains(key)) {
-      accKey != null && !ignoreKey
+    //ignoreKey = skipUnnecessaryKeys (by default is false, true only when "--skip-unnecessary-keys" or "-u")
+    //            AND accKey != null (true when the key is nested)
+    //            AND key is not a preserved keyword (_preservedKeywords)
+
+    // If "--skip-unnecessary-keys" -> ignore every nestested key that is not inside _preservedKeywords
+    if (!ignoreKey) {
+      accKey != null
           ? fileContent +=
               '  static const ${accKey.replaceAll('.', '_')}_$key = \'$accKey.$key\';\n'
-          : !ignoreKey
-              ? fileContent += '  static const $key = \'$key\';\n'
-              : null;
+          : fileContent += '  static const $key = \'$key\';\n';
     }
   }
 
@@ -291,9 +291,9 @@ class CodegenLoader extends AssetLoader{
 // }
 
 void printInfo(String info) {
-  log('\u001b[32measy localization: $info\u001b[0m');
+  stdout.writeln('\u001b[32measy localization: $info\u001b[0m');
 }
 
 void printError(String error) {
-  log('\u001b[31m[ERROR] easy localization: $error\u001b[0m');
+  stderr.writeln('\u001b[31m[ERROR] easy localization: $error\u001b[0m');
 }