b92e34c9bf3b158d1be959edb16f5ea0d5ad555c
Fritze <fritz.matthaeus@hypeit.de>
2025-04-30 08:56:45 +0200

    fix: dart format error in generated key class

--- bin/generate.dart
+++ bin/generate.dart
@@ -177,7 +177,7 @@ Future _writeKeys(StringBuffer classBuilder, List<FileSystemEntity> files,
 
 // ignore_for_file: constant_identifier_names
 
-abstract class  LocaleKeys {
+abstract class LocaleKeys {
 ''';
 
   final fileData = File(files.first.path);
